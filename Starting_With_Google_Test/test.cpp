dae4e269e3dc118cc083e096030711cf0d0b656f
SamuelHenrique32 <shdalmas@ucs.br>
2020-06-03 17:31:05 -0300

    Withdraw test

--- Starting_With_Google_Test/test.cpp
+++ Starting_With_Google_Test/test.cpp
@@ -21,6 +21,16 @@ struct BankAccount {
     
         balance += amount;
     }
+
+    bool withDraw(int amount) {
+
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
 };
 
 // Inherit from Test
@@ -34,7 +44,7 @@ struct BankAccountTest : testing::Test {
     }
 
     // Test destructor
-    ~BankAccountTest() {
+    virtual ~BankAccountTest() {
         delete account;
     }
 };
@@ -54,6 +64,47 @@ TEST_F(BankAccountTest, CanDepositMoney) {
     // Verify if the balance is 100
     EXPECT_EQ(100, account->balance);
 }
+
+struct account_state {
+
+    int initial_balance;
+    int withdraw_amount;
+    int final_balance;
+    bool success;
+
+    friend std::ostream& operator<<(std::ostream& os, const account_state& obj) {
+        return os
+            << "initial_balance: " << obj.initial_balance
+            << " withdraw_amount: " << obj.withdraw_amount
+            << " final_balance: " << obj.final_balance
+            << " success: " << obj.success;
+    }
+};
+
+struct WithDrawAccountTest : BankAccountTest, testing::WithParamInterface<account_state> {
+
+    WithDrawAccountTest() {
+        account->balance = GetParam().initial_balance;
+    }
+};
+
+TEST_P(WithDrawAccountTest, FinalBalance) {
+    
+    auto as = GetParam();
+
+    auto success = account->withDraw(as.withdraw_amount);
+
+    EXPECT_EQ(as.final_balance, account->balance);
+
+    EXPECT_EQ(as.success, success);
+}
+
+// Test cases
+INSTANTIATE_TEST_CASE_P(Default, WithDrawAccountTest,
+    testing::Values(
+        account_state{100,50,50,true},
+        account_state{100,200,100,false}
+));
 
 int main(int argc, char* argv[]) {
 
